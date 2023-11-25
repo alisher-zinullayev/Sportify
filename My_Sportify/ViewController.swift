9583b477461b5097972f6101897d88623d176d49
alisher-zinullayev <zinullayev09@gmail.com>
2023-11-25 17:10:26 +0600

    Initial Commit

---
+++ My_Sportify/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  My_Sportify
+//
+//  Created by Alisher Zinullayev on 25.11.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
